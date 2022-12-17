4b042b1d8f5d822155b51d9a3113e6f702e84c83
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-12-17 17:35:14 +0530

    feat: summary generation

--- chrome-extension/browserAction/background_script.js
+++ chrome-extension/browserAction/background_script.js
@@ -1,1 +1,44 @@
 // Put all the javascript code here, that you want to execute in background.
+let TAB_URL =
+  "https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/";
+const SUMMARIZER_API_URL = "https://api.meaningcloud.com/summarization-1.0";
+let SUMMARY = "";
+
+const SUMMARIZER_OPTIONS = {
+  method: "POST",
+  headers: {
+    Accept: "multipart/form-data",
+  },
+};
+
+// chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//   return (TAB_URL = tabs[0].url);
+// });
+
+async function getSummary() {
+  try {
+    const formdata = new FormData();
+    formdata.append("key", "5b972915e9547b37bae3dcb4c959173e");
+    formdata.append("url", TAB_URL);
+    formdata.append("sentences", "3");
+    const requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    };
+
+    const summaryData = await fetch(SUMMARIZER_API_URL, requestOptions);
+    const summary = await summaryData.json();
+    displaySummary(summary.summary);
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+function displaySummary(summary) {
+  const div = document.querySelector(".summary");
+  div.innerText = summary;
+}
+setTimeout(() => {
+  getSummary();
+}, 100);
--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -1,2 +1,5 @@
 // Put all the javascript code here, that you want to execute after page load.
 
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  console.log(tabs[0].url);
+});
