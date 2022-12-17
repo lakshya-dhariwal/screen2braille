351a88a217d48d4d88c258fbb7c6d8c6757d3e62
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-12-17 17:42:55 +0530

    remove unused variables

--- chrome-extension/browserAction/background_script.js
+++ chrome-extension/browserAction/background_script.js
@@ -2,14 +2,6 @@
 let TAB_URL =
   "https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/";
 const SUMMARIZER_API_URL = "https://api.meaningcloud.com/summarization-1.0";
-let SUMMARY = "";
-
-const SUMMARIZER_OPTIONS = {
-  method: "POST",
-  headers: {
-    Accept: "multipart/form-data",
-  },
-};
 
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   return (TAB_URL = tabs[0].url);
